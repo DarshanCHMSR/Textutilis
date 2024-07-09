4fbfa80d3961eaf4f1b3ba8a1b3bbcb7406dc238
DarshanCHMSR <chdarshan99@gmail.com>
2024-07-09 23:11:27 +0530

    Updates

---
+++ precache-manifest.05ab62742936d4aa691415e4b8700d66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db989b29824abda1ba1ec1998e777f18",
+    "url": "/Textutilis/index.html"
+  },
+  {
+    "revision": "dc30d05037be0f011d54",
+    "url": "/Textutilis/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "c72feb6dc286339a449b",
+    "url": "/Textutilis/static/js/2.38fec76d.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/Textutilis/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "dc30d05037be0f011d54",
+    "url": "/Textutilis/static/js/main.d3c93345.chunk.js"
+  },
+  {
+    "revision": "0ca8dff6dcf3dade684d",
+    "url": "/Textutilis/static/js/runtime~main.3791a3ae.js"
+  }
+]);
